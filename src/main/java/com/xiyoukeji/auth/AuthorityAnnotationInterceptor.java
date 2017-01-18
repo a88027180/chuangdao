86ac19d5625a1bef54ec82bc2f4ddeda38025ded
MatildaX <605830409@qq.com>
2017-01-18 09:52:20 +0800

    取消跨域访问

--- src/main/java/com/xiyoukeji/auth/AuthorityAnnotationInterceptor.java
+++ src/main/java/com/xiyoukeji/auth/AuthorityAnnotationInterceptor.java
@@ -28,10 +28,10 @@ public class AuthorityAnnotationInterceptor extends HandlerInterceptorAdapter {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
         // 允许跨域访问
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+//        response.setHeader("Access-Control-Allow-Origin", "*");
+//        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+//        response.setHeader("Access-Control-Max-Age", "3600");
+//        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
 
         // 权限控制
         if(handler instanceof HandlerMethod) {
